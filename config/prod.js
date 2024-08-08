1bf980e13b07a2d065811886481dc845ab9c9515
Steve Fox <stevefox.dev@gmail.com>
2024-08-08 19:36:51 -0400

    fix google redirect uri

--- config/prod.js
+++ config/prod.js
@@ -2,5 +2,6 @@ module.exports = {
     googleClientID: process.env.GOOGLE_CLIENT_ID,
     googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
     mongoURI: process.env.MONGO_URI,
-    cookieKey: process.env.COOKIE_KEY
+    cookieKey: process.env.COOKIE_KEY,
+    googleRedirectURI: 'https://glacial-anchorage-15396-a039fe345a7d.herokuapp.com/'
 };
